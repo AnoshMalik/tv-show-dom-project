f3a8bcbc133c0791d36682f17f6a884d0c743b2b
Anosh Malik <anoshmalik.am@gmail.com>
2023-01-05 20:23:10 +0000

    Looking aesthetically better Alignments working. Font styles are ok

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const rootElement = document.getElementById("root");
 rootElement.style.display = "flex";
 rootElement.style.flexDirection = "row";
 rootElement.style.flexWrap = "wrap";
+rootElement.style.justifyContent = "center";
+
 
 
 
@@ -34,6 +36,7 @@ function createCard(episode) {
   const episodeName = episode.name;
   let seasonNumber = episode["season"].toString().padStart(2,0);
   let episodeNumber = episode.number.toString().padStart(2, 0);
+  const episodeKey = `${episodeName} \n\ S${seasonNumber} - E${episodeNumber}`;
 
   const container = document.createElement("div");
   const secondaryContainer = document.createElement("div");
@@ -41,18 +44,24 @@ function createCard(episode) {
   const image = document.createElement("img");
   const p = document.createElement("p");
 
-  button.innerText = `${episodeName} - S${seasonNumber} - E${episodeNumber}`; 
+  button.innerText = episodeKey; 
   button.onclick = () => {
     window.location = `${episode.url}`
   };
   button.style.width = "250px";
+  button.style.height = "75px";
+  button.style.borderRadius = "20px 20px 0 0 ";
+
+
   button.style.fontSize = "17px";
+  button.style.fontWeight = "900";
 
   // let pValue = oneEpisode.summary.replaceAll("<p>", "");
   pValue = episode.summary.replaceAll("</p>", "");
   gValue = pValue.replaceAll("<p>","");
   p.innerText = gValue;
-  p.style.color = "black";  
+  p.style.color = "white";
+  p.style.fontFamily = "Impact,Charcoal,sans-serif";
 
 
   image.src = episode.image.medium;
@@ -63,7 +72,15 @@ function createCard(episode) {
   container.appendChild(button);
   container.appendChild(secondaryContainer);
   container.style.width = "250px";
-  container.style.backgroundColor = "beige";
+  container.style.minHeight = "500px";
+  
+  // container.style.maxHeight = "400px";
+
+
+  container.style.backgroundColor = "gray";
+  container.style.margin = "10px";
+  container.style.padding = "10px";
+  container.style.borderRadius = "20px";
   return container;
 
  }

9883f0c087914d0df5f23fd0b8bac191f87678c8
Anosh Malik <anoshmalik.am@gmail.com>
2023-01-05 21:08:09 +0000

    Started level 200

---
+++ search.js
@@ -0,0 +1,12 @@
+const rootElement = document.getElementById("root");
+const searchDiv = document.getElementById("searchDiv");
+const searchBar = document.getElementById("searchBar");
+const searchResults = document.getElementById("searchResults");
+
+function getSearchResults() {
+    
+
+
+}
+ 
+getSearchResults();
